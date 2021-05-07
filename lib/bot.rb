9438dff65ba79be627828140bd5b0435be7d6682
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 14:23:35 +0100

    add list of commands that can be used for the bot

--- lib/bot.rb
+++ lib/bot.rb
@@ -13,17 +13,24 @@ class Bot
         case message.text
         when '/start'
   
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , I am MySickleBot a telegram bot where you can get health tips on Sickle Cell Anaemia. You can get healthy living, harmful things, help a loved one and when to get medical care tips.")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, I am MySickleBot a bot that gives health tips on Sickle Cell Anaemia. How can I help you today?")
   
-          bot.api.send_message(chat_id: message.chat.id, text: 'Here are the list of commands you can run:')
+          bot.api.send_message(chat_id: message.chat.id, text: "Here are the list of commands you can run:
+            \n /helpful_things: List of ways to take care of yourself.
+            \n /harmful_things: List of things to stay away from.
+            \n /dealing_with_pain: Good guidelines to follow when in pain.
+            \n /how_to_help: List of ways you can provide support to a loved one.
+            \n /get_care: Possible problems caused by SCD that needs medical attention right away.
+            \n /commands: Displays the list of commands.
+            \n /bye: Says goodbye ")
   
         when '/helpful_things'
           bot.api.send_message(chat_id: message.chat.id, text: "#{tips_content[2]}")
   
-        when '/stop'
-          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        when '/bye'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name} and take care.")
         end
       end
-    end  
+    end
   end  
 end
