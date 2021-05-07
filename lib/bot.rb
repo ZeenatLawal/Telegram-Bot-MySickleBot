aa91fc232bab2d3fe50073a7912c3920620bfe38
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 12:33:14 +0100

    create instance of scrapper module and health class

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,25 +1,29 @@
 require 'telegram/bot'
+require_relative '../lib/health_tips'
+require './token'
 
 class Bot
+  
   def initialize
-    token = '1812923032:AAGWtd5mUNtHH8f55M4Dj02Zk5QspiNTCK8'
-
-    Telegram::Bot::Client.run(token) do |bot|
+    tips = Scrapper::Health.new
+    tips_title = tips.tips_heading
+    tips_content = tips.tips_content
+    Telegram::Bot::Client.run(TOKEN) do |bot|
       bot.listen do |message|
         case message.text
         when '/start'
-
+  
           bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , I am MySickleBot a telegram bot where you can get health tips on Sickle Cell Anaemia. You can get healthy living, harmful things, help a loved one and when to get medical care tips.")
-
-          bot.api.send_message(chat_id: message.chat.id, text: "Here are the list of commands you can run:")
-
+  
+          bot.api.send_message(chat_id: message.chat.id, text: 'Here are the list of commands you can run:')
+  
+        when '/helpful_things'
+          bot.api.send_message(chat_id: message.chat.id, text: "#{tips_content[2]}")
+  
         when '/stop'
-
-          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-        when '/helpful things'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
         end
       end
-    end
-  end
+    end  
+  end  
 end
