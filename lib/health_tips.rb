f61b4abfc4140302207ec7f819e9be7a069203a3
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 15:57:11 +0100

    fix linter errors

--- lib/health_tips.rb
+++ lib/health_tips.rb
@@ -4,24 +4,23 @@ require 'byebug'
 
 module Scrapper
   class Health
-  
     def tips_heading
       titles_arr = []
-      url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
+      url = 'https://www.webmd.com/a-to-z-guides/living-with-sickle-cell'
       unparsed_page = HTTParty.get(url)
       parsed_page = Nokogiri::HTML(unparsed_page)
-      parsed_page.xpath("//section//h2").each do |title|
+      parsed_page.xpath('//section//h2').each do |title|
         titles_arr << title.text
       end
-      titles_arr 
+      titles_arr
     end
-  
+
     def tips_content
       tips_arr = []
-      url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
+      url = 'https://www.webmd.com/a-to-z-guides/living-with-sickle-cell'
       unparsed_page = HTTParty.get(url)
-      parsed_page = Nokogiri::HTML(unparsed_page) 
-      parsed_page.xpath("//section//ul//li").each do |tip_list|
+      parsed_page = Nokogiri::HTML(unparsed_page)
+      parsed_page.xpath('//section//ul//li').each do |tip_list|
         tips_arr << tip_list.text
       end
       tips_arr
