32ecb21a2b4cc37197ec9bab9ee509ca034962f6
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 09:48:13 +0100

    edit text for /start command in bot class

--- lib/bot.rb
+++ lib/bot.rb
@@ -1,25 +1,25 @@
 require 'telegram/bot'
-require_relative 'health_tips.rb'
-
 
 class Bot
   def initialize
     token = '1812923032:AAGWtd5mUNtHH8f55M4Dj02Zk5QspiNTCK8'
 
-  Telegram::Bot::Client.run(token) do |bot|
-    bot.listen do |message|
-      case message.text
-      when '/start'
+    Telegram::Bot::Client.run(token) do |bot|
+      bot.listen do |message|
+        case message.text
+        when '/start'
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , How are you today?")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name} , I am MySickleBot a telegram bot where you can get health tips on Sickle Cell Anaemia. You can get healthy living, harmful things, help a loved one and when to get medical care tips.")
 
-      when '/stop'
+          bot.api.send_message(chat_id: message.chat.id, text: "Here are the list of commands you can run:")
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
-      when '/helpful things'
-        bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
+        when '/stop'
+
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        when '/helpful things'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+        end
       end
     end
-  end
   end
 end
