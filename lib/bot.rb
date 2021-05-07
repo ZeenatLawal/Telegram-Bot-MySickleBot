758d09fae16375161f66140176be6e2c53fe12b2
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 15:18:23 +0100

    add content to the /how_to_help command and update the list of commands

--- lib/bot.rb
+++ lib/bot.rb
@@ -12,24 +12,11 @@ class Bot
       bot.listen do |message|
         case message.text
         when '/start'
-  
           bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, I am MySickleBot a bot that gives health tips on Sickle Cell Anaemia. How can I help you today? To see the list of commands run /commands.")
 
         when '/welcome'
-  
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, I am MySickleBot a bot that gives health tips on Sickle Cell Anaemia. How can I help you today? To see the list of commands run /commands.")
-
-        when '/commands'    
-          bot.api.send_message(chat_id: message.chat.id, text: "Here is the list of commands you can run:
-            \n /welcome: Welcomes the user.
-            \n /helpful_things: List of ways to take care of yourself.
-            \n /harmful_things: List of things to stay away from.
-            \n /dealing_with_pain: Good guidelines to follow when in pain.
-            \n /how_to_help: List of ways you can provide support to a loved one.
-            \n /get_care: Possible problems caused by SCD that needs medical attention right away.
-            \n /commands: Displays the list of commands.
-            \n /bye: Says goodbye ")
-  
+            bot.api.send_message(chat_id: message.chat.id, text: "Hello #{message.from.first_name}, I am MySickleBot a bot that gives health tips on Sickle Cell Anaemia. How can I help you today? To see the list of commands run /commands.")
+          
         when '/helpful_things'
           bot.api.send_message(chat_id: message.chat.id, text: "#{tips_title[1]} - List of ways to take care of yourself
             \n - #{tips_content[4]}
@@ -48,14 +35,34 @@ class Bot
             \n - #{tips_content[14]}
             \n - #{tips_content[15]}
             \n - #{tips_content[16]}")
-
+            
         when '/dealing_with_pain'
           bot.api.send_message(chat_id: message.chat.id, text: "#{tips_title[0]} - Good guidelines to follow when in pain
             \n - #{tips_content[0]}
             \n - #{tips_content[1]}
             \n - #{tips_content[2]}
             \n - #{tips_content[3]}")
-  
+          
+        when '/how_to_help'
+          bot.api.send_message(chat_id: message.chat.id, text: "#{tips_title[3]} - List of ways you can provide support to a loved one
+            \n - #{tips_content[17]}
+            \n - #{tips_content[18]}
+            \n - #{tips_content[19]}
+            \n - #{tips_content[20]}
+            \n - #{tips_content[21]}")
+            
+        when '/commands'    
+          bot.api.send_message(chat_id: message.chat.id, text: "Here is the list of commands you can run:
+            \n /welcome: Welcomes the user.
+            \n /helpful_things: List of ways to take care of yourself.
+            \n /harmful_things: List of things to stay away from.
+            \n /dealing_with_pain: Good guidelines to follow when in pain.
+            \n /how_to_help: List of ways you can provide support to a loved one.
+            \n /get_care: Possible problems caused by SCD that needs medical attention right away.
+            \n /commands: Displays the list of commands.
+            \n /bye: Says goodbye 
+            \n /webmd: Link to the website where the tips were gotten from")
+
         when '/bye'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name} and take care.")
         end
