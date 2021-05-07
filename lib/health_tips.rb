891562c891af694370a7e7a3208f9d7bd154c52a
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-07 12:32:21 +0100

    remove unused methods in health class

--- lib/health_tips.rb
+++ lib/health_tips.rb
@@ -2,40 +2,29 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 
-class Health
-
-  def initialize
-    @tips = []
-    @titles = []
-  end
-
-  def health_tips_title
-    @titles << tips_heading
-  end
-
-  def health_tips
-    @tips << tips_content
-  end
-
-  def tips_heading
-    titles_arr = []
-    url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page)
-    parsed_page.xpath("//section//h2").each do |title|
-      titles_arr << title.text
+module Scrapper
+  class Health
+  
+    def tips_heading
+      titles_arr = []
+      url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
+      unparsed_page = HTTParty.get(url)
+      parsed_page = Nokogiri::HTML(unparsed_page)
+      parsed_page.xpath("//section//h2").each do |title|
+        titles_arr << title.text
+      end
+      titles_arr 
     end
-    titles_arr 
-  end
-
-  def tips_content
-    tips_arr = []
-    url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
-    unparsed_page = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(unparsed_page) 
-    parsed_page.xpath("//section//ul//li").each do |tip_list|
-      tips_arr << tip_list.text
+  
+    def tips_content
+      tips_arr = []
+      url = "https://www.webmd.com/a-to-z-guides/living-with-sickle-cell"
+      unparsed_page = HTTParty.get(url)
+      parsed_page = Nokogiri::HTML(unparsed_page) 
+      parsed_page.xpath("//section//ul//li").each do |tip_list|
+        tips_arr << tip_list.text
+      end
+      tips_arr
     end
-    tips_arr
   end
 end
