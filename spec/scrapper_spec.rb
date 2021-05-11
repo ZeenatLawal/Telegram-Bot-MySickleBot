c588ec80ea5829c3d1ae431f80a57362500e51d2
ZeenatLawal <zeenatlawal82@gmail.com>
2021-05-11 07:54:22 +0100

    rename test methods in rspec file

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -3,39 +3,39 @@ require './lib/scrapper'
 module Scrapper
   describe Health do
     let(:title) { Scrapper::Health.new }
-    describe '#tips_heading' do
+    describe '#health_tips_title' do
       it 'returns an array of titles' do
-        expect(title.tips_heading).to be_a Array
+        expect(title.health_tips_title).to be_a Array
       end
 
       it 'return false if array is empty' do
-        expect(title.tips_heading.nil?).to be false
+        expect(title.health_tips_title.nil?).to be false
       end
 
       it 'return false if array contains Integers' do
-        expect(title.tips_heading.is_a?(Integer)).to be false
+        expect(title.health_tips_title.is_a?(Integer)).to be false
       end
 
       it 'return true if array contains Strings' do
-        expect(title.tips_heading.all?(String)).to be true
+        expect(title.health_tips_title.all?(String)).to be true
       end
     end
 
-    describe '#tips_content' do
+    describe '#health_tips' do
       it 'returns an array of content' do
-        expect(title.tips_content).to be_a Array
+        expect(title.health_tips).to be_a Array
       end
 
       it 'return false if array is empty' do
-        expect(title.tips_content.nil?).to be false
+        expect(title.health_tips.nil?).to be false
       end
 
       it 'return false if array contains Integers' do
-        expect(title.tips_content.is_a?(Integer)).to be false
+        expect(title.health_tips.is_a?(Integer)).to be false
       end
 
       it 'return true if array contains Strings' do
-        expect(title.tips_content.all?(String)).to be true
+        expect(title.health_tips.all?(String)).to be true
       end
     end
   end
